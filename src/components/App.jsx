ebcbff9783709bfd8ad7148ffa5853675b6dd909
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-03 07:59:17 +0200

    Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import React from "react";
+import "../styles/index.css";
+import Hello from "./Hello";
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-green-400 text-4xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default Hello;
