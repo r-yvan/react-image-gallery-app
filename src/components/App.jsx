aa5de70b5c1a4114a6b21af925b0d9c63f283252
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-05 12:28:25 +0200

    Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,12 @@ import Nature from "./Nature";
 import Military from "./Military";
 import Fantasy from "./Fantasy";
 import Animals from "./Animals";
+import Cars from "./Cars";
+import Minimal from "./Minimal";
+import City from "./City";
+import Artistic from "./Artistic";
+import Cyberpunk from "./Cyberpunk";
+
 const App = () => {
   return (
     <Router>
@@ -22,10 +28,15 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/overview" element={<Overview />} />
           <Route path="/categories" element={<Categories />} />
-          <Route path="/categories/nature" element={<Nature/>} />
-          <Route path="/categories/military" element={<Military/>} />
-          <Route path="/categories/fantasy" element={<Fantasy/>} />
-          <Route path="/categories/animals" element={<Animals/>} />
+          <Route path="/categories/nature" element={<Nature />} />
+          <Route path="/categories/military" element={<Military />} />
+          <Route path="/categories/fantasy" element={<Fantasy />} />
+          <Route path="/categories/animals" element={<Animals />} />
+          <Route path="/categories/cars" element={<Cars />} />
+          <Route path="/categories/minimal" element={<Minimal />} />
+          <Route path="/categories/city" element={<City />} />
+          <Route path="/categories/artistic" element={<Artistic />} />
+          <Route path="/categories/cyberpunk" element={<Cyberpunk />} />
         </Route>
       </Routes>
     </Router>
--- src/components/Fantasy.jsx
+++ src/components/Fantasy.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+const images = import.meta.glob("../assets/fantasy/*.{png,jpg,jpeg,svg}");
 
 const Fantasy = () => {
-  return (
-    <div>Fantasy</div>
-  )
-}
+  const [fantasyImages, setFantasyImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isImageSelected, setIsImageSelected] = useState(false);
 
-export default Fantasy
+  useEffect(() => {
+    const loadImages = async () => {
+      const imagePaths = await Promise.all(
+        Object.keys(images).map(async (path) => {
+          const module = await images[path]();
+          return module.default;
+        })
+      );
+      setFantasyImages(imagePaths);
+    };
+    loadImages();
+  }, []);
+
+  useEffect(() => {
+    if (isImageSelected) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isImageSelected]);
+
+  return (
+    <div className="text-white font-body bg-body">
+      <div className="bg-component mt-2 mx-2 rounded-2xl">
+        <div className="flex justify-center text-4xl font-bold mb-10 rounded-2xl pt-4">
+          <h2>Fantasy</h2>
+        </div>
+        <div className="flex flex-wrap flex-row gap-10 w-full h-full pl-16">
+          {fantasyImages.map((image, index) => (
+            <div
+              key={index}
+              className="flex flex-col flex-shrink-0 bg-card rounded-2xl p-2 card-width border-white border-2 border-opacity-15 cursor-pointer"
+              onClick={() => {
+                setSelectedImage(image);
+                setIsImageSelected(true);
+              }}
+            >
+              <div className="w-full rounded-2xl overflow-hidden">
+                <img src={image} alt={`Fantasy ${index}`} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {isImageSelected && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50"
+          onClick={() => setIsImageSelected(false)} 
+        >
+          <div
+            className="relative bg-component rounded-2xl shadow-lg overflow-hidden p-2"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <img
+              src={selectedImage}
+              alt="Selected"
+              className="rounded-2xl selected-image-width"
+            />
+            <button
+              className="absolute top-5 right-7 py-1 px-3 rounded-full border-2 border-white border-opacity-20 text-white bg-blue-500 bg-opacity-20 font-bold text-2xl"
+              onClick={() => setIsImageSelected(false)}
+            >
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Fantasy;
