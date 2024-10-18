2611f08bec7a639fb07c294cf29c394fd58ac043
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-18 23:40:01 +0200

    Commit

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,85 +1,85 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const images = import.meta.glob("../assets/city/*.{png,jpg,jpeg,svg}");
+// const images = import.meta.glob("../assets/city/*.{png,jpg,jpeg,svg}");
 
-const City = () => {
-  const [cityImages, setCityImages] = useState([]);
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [isImageSelected, setIsImageSelected] = useState(false);
+// const City = () => {
+//   const [cityImages, setCityImages] = useState([]);
+//   const [selectedImage, setSelectedImage] = useState(null);
+//   const [isImageSelected, setIsImageSelected] = useState(false);
 
-  useEffect(() => {
-    const loadImages = async () => {
-      const imagePaths = await Promise.all(
-        Object.keys(images).map(async (path) => {
-          const module = await images[path]();
-          return module.default;
-        })
-      );
-      setCityImages(imagePaths);
-    };
-    loadImages();
-  }, []);
+//   useEffect(() => {
+//     const loadImages = async () => {
+//       const imagePaths = await Promise.all(
+//         Object.keys(images).map(async (path) => {
+//           const module = await images[path]();
+//           return module.default;
+//         })
+//       );
+//       setCityImages(imagePaths);
+//     };
+//     loadImages();
+//   }, []);
 
-  useEffect(() => {
-    if (isImageSelected) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [isImageSelected]);
+//   useEffect(() => {
+//     if (isImageSelected) {
+//       document.body.style.overflow = "hidden";
+//     } else {
+//       document.body.style.overflow = "auto";
+//     }
+//     return () => {
+//       document.body.style.overflow = "auto";
+//     };
+//   }, [isImageSelected]);
 
-  return (
-    <div className="text-white font-body bg-body">
-      <div className="bg-component mt-2 mx-2 rounded-2xl pb-10">
-        <div className="flex justify-center text-4xl font-bold mb-10 rounded-2xl pt-4">
-          <h2>City</h2>
-        </div>
-        <div className="flex flex-wrap flex-row gap-10 w-full h-full pl-16">
-          {cityImages.map((image, index) => (
-            <div
-              key={index}
-              className="flex flex-col flex-shrink-0 bg-card rounded-2xl p-2 card-width border-white border-2 border-opacity-15 cursor-pointer"
-              onClick={() => {
-                setSelectedImage(image);
-                setIsImageSelected(true);
-              }}
-            >
-              <div className="w-full rounded-2xl overflow-hidden">
-                <img src={image} alt={`City ${index}`} />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+//   return (
+//     <div className="text-white font-body bg-body">
+//       <div className="bg-component mt-2 mx-2 rounded-2xl pb-10">
+//         <div className="flex justify-center text-4xl font-bold mb-10 rounded-2xl pt-4">
+//           <h2>City</h2>
+//         </div>
+//         <div className="flex flex-wrap flex-row gap-10 w-full h-full pl-16">
+//           {cityImages.map((image, index) => (
+//             <div
+//               key={index}
+//               className="flex flex-col flex-shrink-0 bg-card rounded-2xl p-2 card-width border-white border-2 border-opacity-15 cursor-pointer"
+//               onClick={() => {
+//                 setSelectedImage(image);
+//                 setIsImageSelected(true);
+//               }}
+//             >
+//               <div className="w-full rounded-2xl overflow-hidden">
+//                 <img src={image} alt={`City ${index}`} />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
 
-      {isImageSelected && (
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50"
-          onClick={() => setIsImageSelected(false)}
-        >
-          <div
-            className="relative bg-component rounded-2xl shadow-lg overflow-hidden p-2"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <img
-              src={selectedImage}
-              alt="Selected"
-              className="rounded-2xl selected-image-width"
-            />
-            <button
-              className="absolute top-5 right-7 py-1 px-3 rounded-full border-2 border-white border-opacity-20 text-white bg-blue-500 bg-opacity-20 font-bold text-2xl"
-              onClick={() => setIsImageSelected(false)}
-            >
-              &times;
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+//       {isImageSelected && (
+//         <div
+//           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50"
+//           onClick={() => setIsImageSelected(false)}
+//         >
+//           <div
+//             className="relative bg-component rounded-2xl shadow-lg overflow-hidden p-2"
+//             onClick={(e) => e.stopPropagation()}
+//           >
+//             <img
+//               src={selectedImage}
+//               alt="Selected"
+//               className="rounded-2xl selected-image-width"
+//             />
+//             <button
+//               className="absolute top-5 right-7 py-1 px-3 rounded-full border-2 border-white border-opacity-20 text-white bg-blue-500 bg-opacity-20 font-bold text-2xl"
+//               onClick={() => setIsImageSelected(false)}
+//             >
+//               &times;
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-export default City;
+// export default City;
